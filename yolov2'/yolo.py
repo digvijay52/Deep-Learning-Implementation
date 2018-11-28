75093b27be87816384bb8d8bb2c107069627f1bb
digvijay52 <45414942+digvijay52@users.noreply.github.com>
2018-11-28 17:08:24 +0530

    Add files via upload

---
+++ yolov2'/yolo.py
@@ -0,0 +1,439 @@
+from keras.models import Sequential, Model
+from keras.layers import Reshape, Activation, Conv2D, Input, MaxPooling2D, BatchNormalization, Flatten, Dense, Lambda
+from keras.layers.advanced_activations import LeakyReLU
+from keras.callbacks import EarlyStopping, ModelCheckpoint, TensorBoard
+from keras.optimizers import SGD, Adam, RMSprop
+from keras.layers.merge import concatenate
+import matplotlib.pyplot as plt
+import keras.backend as K
+import tensorflow as tf
+import imgaug as ia
+from tqdm import tqdm
+from imgaug import augmenters as iaa
+import numpy as np
+import pickle
+import os, cv2
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+
+
+
+
+
+
+LABELS = ['person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
+
+IMAGE_H, IMAGE_W = 416, 416
+GRID_H,  GRID_W  = 13 , 13
+BOX              = 5
+CLASS            = len(LABELS)
+CLASS_WEIGHTS    = np.ones(CLASS, dtype='float32')
+OBJ_THRESHOLD    = 0.3#0.5
+NMS_THRESHOLD    = 0.3#0.45
+ANCHORS          = [0.57273, 0.677385, 1.87446, 2.06253, 3.33843, 5.47434, 7.88282, 3.52778, 9.77052, 9.16828]
+
+NO_OBJECT_SCALE  = 1.0
+OBJECT_SCALE     = 5.0
+COORD_SCALE      = 1.0
+CLASS_SCALE      = 1.0
+
+BATCH_SIZE       = 16
+WARM_UP_BATCHES  = 0
+TRUE_BOX_BUFFER  = 50
+
+
+wt_path = 'yolov2.weights'   
+def space_to_depth_x2(x):
+    return tf.space_to_depth(x, block_size=2)
+	
+input_image = Input(shape=(IMAGE_H, IMAGE_W, 3))
+true_boxes  = Input(shape=(1, 1, 1, TRUE_BOX_BUFFER , 4))
+
+# Layer 1
+x = Conv2D(32, (3,3), strides=(1,1), padding='same', name='conv_1', use_bias=False)(input_image)
+x = BatchNormalization(name='norm_1')(x)
+x = LeakyReLU(alpha=0.1)(x)
+x = MaxPooling2D(pool_size=(2, 2))(x)
+
+# Layer 2
+x = Conv2D(64, (3,3), strides=(1,1), padding='same', name='conv_2', use_bias=False)(x)
+x = BatchNormalization(name='norm_2')(x)
+x = LeakyReLU(alpha=0.1)(x)
+x = MaxPooling2D(pool_size=(2, 2))(x)
+
+# Layer 3
+x = Conv2D(128, (3,3), strides=(1,1), padding='same', name='conv_3', use_bias=False)(x)
+x = BatchNormalization(name='norm_3')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 4
+x = Conv2D(64, (1,1), strides=(1,1), padding='same', name='conv_4', use_bias=False)(x)
+x = BatchNormalization(name='norm_4')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 5
+x = Conv2D(128, (3,3), strides=(1,1), padding='same', name='conv_5', use_bias=False)(x)
+x = BatchNormalization(name='norm_5')(x)
+x = LeakyReLU(alpha=0.1)(x)
+x = MaxPooling2D(pool_size=(2, 2))(x)
+
+# Layer 6
+x = Conv2D(256, (3,3), strides=(1,1), padding='same', name='conv_6', use_bias=False)(x)
+x = BatchNormalization(name='norm_6')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 7
+x = Conv2D(128, (1,1), strides=(1,1), padding='same', name='conv_7', use_bias=False)(x)
+x = BatchNormalization(name='norm_7')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 8
+x = Conv2D(256, (3,3), strides=(1,1), padding='same', name='conv_8', use_bias=False)(x)
+x = BatchNormalization(name='norm_8')(x)
+x = LeakyReLU(alpha=0.1)(x)
+x = MaxPooling2D(pool_size=(2, 2))(x)
+
+# Layer 9
+x = Conv2D(512, (3,3), strides=(1,1), padding='same', name='conv_9', use_bias=False)(x)
+x = BatchNormalization(name='norm_9')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 10
+x = Conv2D(256, (1,1), strides=(1,1), padding='same', name='conv_10', use_bias=False)(x)
+x = BatchNormalization(name='norm_10')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 11
+x = Conv2D(512, (3,3), strides=(1,1), padding='same', name='conv_11', use_bias=False)(x)
+x = BatchNormalization(name='norm_11')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 12
+x = Conv2D(256, (1,1), strides=(1,1), padding='same', name='conv_12', use_bias=False)(x)
+x = BatchNormalization(name='norm_12')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 13
+x = Conv2D(512, (3,3), strides=(1,1), padding='same', name='conv_13', use_bias=False)(x)
+x = BatchNormalization(name='norm_13')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+skip_connection = x
+
+x = MaxPooling2D(pool_size=(2, 2))(x)
+
+# Layer 14
+x = Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_14', use_bias=False)(x)
+x = BatchNormalization(name='norm_14')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 15
+x = Conv2D(512, (1,1), strides=(1,1), padding='same', name='conv_15', use_bias=False)(x)
+x = BatchNormalization(name='norm_15')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 16
+x = Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_16', use_bias=False)(x)
+x = BatchNormalization(name='norm_16')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 17
+x = Conv2D(512, (1,1), strides=(1,1), padding='same', name='conv_17', use_bias=False)(x)
+x = BatchNormalization(name='norm_17')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 18
+x = Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_18', use_bias=False)(x)
+x = BatchNormalization(name='norm_18')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 19
+x = Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_19', use_bias=False)(x)
+x = BatchNormalization(name='norm_19')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 20
+x = Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_20', use_bias=False)(x)
+x = BatchNormalization(name='norm_20')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 21
+skip_connection = Conv2D(64, (1,1), strides=(1,1), padding='same', name='conv_21', use_bias=False)(skip_connection)
+skip_connection = BatchNormalization(name='norm_21')(skip_connection)
+skip_connection = LeakyReLU(alpha=0.1)(skip_connection)
+skip_connection = Lambda(space_to_depth_x2)(skip_connection)
+
+x = concatenate([skip_connection, x])
+
+# Layer 22
+x = Conv2D(1024, (3,3), strides=(1,1), padding='same', name='conv_22', use_bias=False)(x)
+x = BatchNormalization(name='norm_22')(x)
+x = LeakyReLU(alpha=0.1)(x)
+
+# Layer 23
+x = Conv2D(BOX * (4 + 1 + CLASS), (1,1), strides=(1,1), padding='same', name='conv_23')(x)
+output = Reshape((GRID_H, GRID_W, BOX, 4 + 1 + CLASS))(x)
+
+# small hack to allow true_boxes to be registered when Keras build the model 
+# for more information: https://github.com/fchollet/keras/issues/2790
+output = Lambda(lambda args: args[0])([output, true_boxes])
+
+model = Model([input_image, true_boxes], output)
+
+
+model.summary()
+
+class WeightReader:
+    def __init__(self, weight_file):
+        self.offset = 4
+        self.all_weights = np.fromfile(weight_file, dtype='float32')
+        
+    def read_bytes(self, size):
+        self.offset = self.offset + size
+        return self.all_weights[self.offset-size:self.offset]
+    
+    def reset(self):
+        self.offset = 4
+
+weight_reader = WeightReader(wt_path)
+
+weight_reader.reset()
+nb_conv = 23
+
+for i in range(1, nb_conv+1):
+    conv_layer = model.get_layer('conv_' + str(i))
+    
+    if i < nb_conv:
+        norm_layer = model.get_layer('norm_' + str(i))
+        
+        size = np.prod(norm_layer.get_weights()[0].shape)
+
+        beta  = weight_reader.read_bytes(size)
+        gamma = weight_reader.read_bytes(size)
+        mean  = weight_reader.read_bytes(size)
+        var   = weight_reader.read_bytes(size)
+
+        weights = norm_layer.set_weights([gamma, beta, mean, var])       
+        
+    if len(conv_layer.get_weights()) > 1:
+        bias   = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[1].shape))
+        kernel = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[0].shape))
+        kernel = kernel.reshape(list(reversed(conv_layer.get_weights()[0].shape)))
+        kernel = kernel.transpose([2,3,1,0])
+        conv_layer.set_weights([kernel, bias])
+    else:
+        kernel = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[0].shape))
+        kernel = kernel.reshape(list(reversed(conv_layer.get_weights()[0].shape)))
+        kernel = kernel.transpose([2,3,1,0])
+        conv_layer.set_weights([kernel])
+	
+
+def decode_netout(netout, anchors, nb_class, obj_threshold=0.3, nms_threshold=0.3):
+    grid_h, grid_w, nb_box = netout.shape[:3]
+
+    boxes = []
+    
+   
+    netout[..., 4]  = _sigmoid(netout[..., 4])
+    netout[..., 5:] = netout[..., 4][..., np.newaxis] * _softmax(netout[..., 5:])
+    netout[..., 5:] *= netout[..., 5:] > obj_threshold
+    
+    for row in range(grid_h):
+        for col in range(grid_w):
+            for b in range(nb_box):
+                
+                classes = netout[row,col,b,5:]
+                
+                if np.sum(classes) > 0:
+                    
+                    x, y, w, h = netout[row,col,b,:4]
+
+                    x = (col + _sigmoid(x)) / grid_w 
+                    y = (row + _sigmoid(y)) / grid_h
+                    w = anchors[2 * b + 0] * np.exp(w) / grid_w 
+                    h = anchors[2 * b + 1] * np.exp(h) / grid_h
+                    confidence = netout[row,col,b,4]
+                    
+                    box = BoundBox(x-w/2, y-h/2, x+w/2, y+h/2, confidence, classes)
+                    
+                    boxes.append(box)
+
+    
+    for c in range(nb_class):
+        sorted_indices = list(reversed(np.argsort([box.classes[c] for box in boxes])))
+
+        for i in range(len(sorted_indices)):
+            index_i = sorted_indices[i]
+            
+            if boxes[index_i].classes[c] == 0: 
+                continue
+            else:
+                for j in range(i+1, len(sorted_indices)):
+                    index_j = sorted_indices[j]
+                    
+                    if bbox_iou(boxes[index_i], boxes[index_j]) >= nms_threshold:
+                        boxes[index_j].classes[c] = 0
+                        
+   
+    boxes = [box for box in boxes if box.get_score() > obj_threshold]
+    
+    return boxes    
+
+
+class BoundBox:
+    def __init__(self, xmin, ymin, xmax, ymax, c = None, classes = None):
+        self.xmin = xmin
+        self.ymin = ymin
+        self.xmax = xmax
+        self.ymax = ymax
+        
+        self.c     = c
+        self.classes = classes
+
+        self.label = -1
+        self.score = -1
+
+    def get_label(self):
+        if self.label == -1:
+            self.label = np.argmax(self.classes)
+        
+        return self.label
+    
+    def get_score(self):
+        if self.score == -1:
+            self.score = self.classes[self.get_label()]
+            
+        return self.score
+
+
+def bbox_iou(box1, box2):
+    intersect_w = _interval_overlap([box1.xmin, box1.xmax], [box2.xmin, box2.xmax])
+    intersect_h = _interval_overlap([box1.ymin, box1.ymax], [box2.ymin, box2.ymax])  
+    
+    intersect = intersect_w * intersect_h
+
+    w1, h1 = box1.xmax-box1.xmin, box1.ymax-box1.ymin
+    w2, h2 = box2.xmax-box2.xmin, box2.ymax-box2.ymin
+    
+    union = w1*h1 + w2*h2 - intersect
+    
+    return float(intersect) / union
+
+def draw_boxes(image, boxes, labels):
+    image_h, image_w, _ = image.shape
+
+    for box in boxes:
+        xmin = int(box.xmin*image_w)
+        ymin = int(box.ymin*image_h)
+        xmax = int(box.xmax*image_w)
+        ymax = int(box.ymax*image_h)
+
+        cv2.rectangle(image, (xmin,ymin), (xmax,ymax), (0,255,0), 3)
+        cv2.putText(image, 
+                    labels[box.get_label()] + ' ' + str(box.get_score()), 
+                    (xmin, ymin - 13), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 
+                    2*1e-3*image_h, 
+                    (0,255,0), 2)
+        
+    return image 
+
+def compute_overlap(a, b):
+   
+    area = (b[:, 2] - b[:, 0]) * (b[:, 3] - b[:, 1])
+
+    iw = np.minimum(np.expand_dims(a[:, 2], axis=1), b[:, 2]) - np.maximum(np.expand_dims(a[:, 0], 1), b[:, 0])
+    ih = np.minimum(np.expand_dims(a[:, 3], axis=1), b[:, 3]) - np.maximum(np.expand_dims(a[:, 1], 1), b[:, 1])
+
+    iw = np.maximum(iw, 0)
+    ih = np.maximum(ih, 0)
+
+    ua = np.expand_dims((a[:, 2] - a[:, 0]) * (a[:, 3] - a[:, 1]), axis=1) + area - iw * ih
+
+    ua = np.maximum(ua, np.finfo(float).eps)
+
+    intersection = iw * ih
+
+    return intersection / ua  
+    
+def compute_ap(recall, precision):
+    
+    mrec = np.concatenate(([0.], recall, [1.]))
+    mpre = np.concatenate(([0.], precision, [0.]))
+
+   
+    for i in range(mpre.size - 1, 0, -1):
+        mpre[i - 1] = np.maximum(mpre[i - 1], mpre[i])
+
+   
+    i = np.where(mrec[1:] != mrec[:-1])[0]
+
+   
+    ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
+    return ap      
+        
+def _interval_overlap(interval_a, interval_b):
+    x1, x2 = interval_a
+    x3, x4 = interval_b
+
+    if x3 < x1:
+        if x4 < x1:
+            return 0
+        else:
+            return min(x2,x4) - x1
+    else:
+        if x2 < x3:
+             return 0
+        else:
+            return min(x2,x4) - x3          
+
+def _sigmoid(x):
+    return 1. / (1. + np.exp(-x))
+
+def _softmax(x, axis=-1, t=-100.):
+    x = x - np.max(x)
+    
+    if np.min(x) < t:
+        x = x/np.min(x)*t
+        
+    e_x = np.exp(x)
+    
+    return e_x / e_x.sum(axis, keepdims=True)
+
+dummy_array = np.zeros((1,1,1,1,TRUE_BOX_BUFFER,4))
+
+video_inp = 'test.mp4'
+video_out = 'output.mp4'
+
+video_reader = cv2.VideoCapture(video_inp)
+
+nb_frames = int(video_reader.get(cv2.CAP_PROP_FRAME_COUNT))
+frame_h = int(video_reader.get(cv2.CAP_PROP_FRAME_HEIGHT))
+frame_w = int(video_reader.get(cv2.CAP_PROP_FRAME_WIDTH))
+
+video_writer = cv2.VideoWriter(video_out,
+                               cv2.VideoWriter_fourcc(*'XVID'), 
+                               30.0, 
+                               (frame_w, frame_h))
+for i in tqdm(range(nb_frames)):
+    ret, image = video_reader.read()
+    
+    input_image = cv2.resize(image, (416, 416))
+    input_image = input_image / 255.
+    input_image = input_image[:,:,::-1]
+    input_image = np.expand_dims(input_image, 0)
+
+    netout = model.predict([input_image, dummy_array])
+
+    boxes = decode_netout(netout[0], 
+                          obj_threshold=0.3,
+                          nms_threshold=NMS_THRESHOLD,
+                          anchors=ANCHORS, 
+                          nb_class=CLASS)
+    image = draw_boxes(image, boxes, labels=LABELS)
+    
+    video_writer.write(np.uint8(image))
+    
+video_reader.release()
+video_writer.release()  
